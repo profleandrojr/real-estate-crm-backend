b37b07c8ee7a4d1238017617be7c21662bd81875
Leandro Junior Alves dos Santos <prof.leandrojr@gmail.com>
2025-12-23 20:17:42 -0300

    GNU licence inside code

--- src/main/java/com/realestate/crm_backend/CrmBackendApplication.java
+++ src/main/java/com/realestate/crm_backend/CrmBackendApplication.java
@@ -1,3 +1,20 @@
+/*
+ * Real Estate CRM - High-performance Real Estate Management Platform
+ * Copyright (C) 2025 Leandro Junior Alves dos Santos
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.realestate.crm_backend;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +23,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrmBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrmBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrmBackendApplication.class, args);
+    }
 
 }
--- src/main/java/com/realestate/crm_backend/entity/Listing.java
+++ src/main/java/com/realestate/crm_backend/entity/Listing.java
@@ -1,3 +1,20 @@
+/*
+ * Real Estate CRM - High-performance Real Estate Management Platform
+ * Copyright (C) 2025 Leandro Junior Alves dos Santos
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.realestate.crm_backend.entity;
 
 import java.math.BigDecimal;
